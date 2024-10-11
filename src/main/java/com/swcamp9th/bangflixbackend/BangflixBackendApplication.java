fb4500a3b92fdd48f6530f3f7ef7e6b58bb13765
JeongJunSeo99 <wnstj0810@naver.com>
2024-10-11 12:20:16 +0900

    config: 프로젝트 생성

---
+++ src/main/java/com/swcamp9th/bangflixbackend/BangflixBackendApplication.java
@@ -0,0 +1,13 @@
+package com.swcamp9th.bangflixbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BangflixBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BangflixBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swcamp9th/bangflixbackend/BangflixBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swcamp9th.bangflixbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BangflixBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
