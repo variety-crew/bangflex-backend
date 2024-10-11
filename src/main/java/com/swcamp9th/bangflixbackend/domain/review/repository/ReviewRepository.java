e383547367bf29e7e6254b9b09e6fc24fd019837
JeongJunSeo99 <wnstj0810@naver.com>
2024-10-11 18:58:05 +0900

    feat: 리뷰 작성 기능 개발

--- src/main/java/com/swcamp9th/bangflixbackend/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/swcamp9th/bangflixbackend/domain/review/repository/ReviewRepository.java
@@ -1,5 +1,10 @@
 package com.swcamp9th.bangflixbackend.domain.review.repository;
 
-public class ReviewRepository {
+import com.swcamp9th.bangflixbackend.domain.review.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
 
 }
