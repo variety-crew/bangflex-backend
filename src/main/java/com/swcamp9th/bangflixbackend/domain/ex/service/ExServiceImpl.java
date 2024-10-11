c043d9db2eb4e70efbb18eaf7be77188a3bbcd04
JeongJunSeo99 <82440364+JeongJunSeo99@users.noreply.github.com>
2024-10-11 18:13:46 +0900

    Update and rename ExService.java to ExServiceImpl.java

--- src/main/java/com/swcamp9th/bangflixbackend/domain/ex/service/ExServiceImpl.java
+++ src/main/java/com/swcamp9th/bangflixbackend/domain/ex/service/ExServiceImpl.java
@@ -3,6 +3,6 @@ package com.swcamp9th.bangflixbackend.domain.ex.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ExService {
+public class ExServiceImpl implements ExService {
 
 }
