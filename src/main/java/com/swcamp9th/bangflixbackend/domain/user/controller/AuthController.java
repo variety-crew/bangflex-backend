1f1b935461134b6bb257826d8bd5034e19a21539
Rohdamin <rohdamin3@gmail.com>
2024-10-13 00:47:47 +0900

    feat: 인증 관련 API 컨트롤러 이름 변경, 리턴값 변경, API summary 추가

---
+++ src/main/java/com/swcamp9th/bangflixbackend/domain/user/controller/AuthController.java
@@ -0,0 +1,45 @@
+package com.swcamp9th.bangflixbackend.domain.user.controller;
+
+import com.swcamp9th.bangflixbackend.common.ResponseMessage;
+import com.swcamp9th.bangflixbackend.domain.user.dto.*;
+import com.swcamp9th.bangflixbackend.domain.user.service.UserServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final UserServiceImpl userService;
+
+    @PostMapping("/signup")
+    @Operation(summary = "회원가입 API")
+    public ResponseEntity<ResponseMessage<Object>> signup(@Valid @RequestBody SignupRequestDto signupRequestDto) {
+        return ResponseEntity.ok(new ResponseMessage<>(200, "회원 가입 성공", userService.signup(signupRequestDto)));
+    }
+
+    @PostMapping("/login")
+    @Operation(summary = "로그인 API")
+    public ResponseEntity<ResponseMessage<Object>> login(@Valid @RequestBody SignRequestDto signRequestDto) {
+        return ResponseEntity.ok(new ResponseMessage<>(200, "로그인 성공", userService.login(signRequestDto)));
+    }
+
+    @PostMapping("/logout")
+    @Operation(summary = "로그아웃 API")
+    public ResponseEntity<ResponseMessage<Object>> logout(@Valid @RequestBody RefreshTokenRequestDto refreshTokenRequestDto) {
+        userService.logout(refreshTokenRequestDto.getRefreshToken());
+        return ResponseEntity.ok(new ResponseMessage<>(200, "로그아웃 성공", null));
+    }
+
+    @PostMapping("/refresh")
+    @Operation(summary = "엑세스 토큰 재발급 API")
+    public ResponseEntity<ResponseMessage<Object>> refresh(@Valid @RequestBody RefreshTokenRequestDto refreshTokenRequestDto) {
+        return ResponseEntity.ok(new ResponseMessage<>(200, "엑세스 토큰 재발급 성공", userService.refreshTokens(refreshTokenRequestDto.getRefreshToken())));
+    }
+}
