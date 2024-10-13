6d39207afbd6f0f160023451c69debbb0349d9c1
Rohdamin <rohdamin3@gmail.com>
2024-10-13 14:54:22 +0900

    feat: 회원 정보 조회 컨트롤러 추가

--- src/main/java/com/swcamp9th/bangflixbackend/domain/user/controller/UserController.java
+++ src/main/java/com/swcamp9th/bangflixbackend/domain/user/controller/UserController.java
@@ -1,45 +1,43 @@
 package com.swcamp9th.bangflixbackend.domain.user.controller;
 
+import com.swcamp9th.bangflixbackend.common.ResponseMessage;
 import com.swcamp9th.bangflixbackend.domain.user.dto.*;
 import com.swcamp9th.bangflixbackend.domain.user.service.UserServiceImpl;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/auth")
+@RequestMapping("/user")
+@Slf4j
 public class UserController {
 
     private final UserServiceImpl userService;
 
-    @PostMapping("/signup")
-    public ResponseEntity<SignupResponseDto> signup(@Valid @RequestBody SignupRequestDto signupRequestDto) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(userService.signup(signupRequestDto));
-    }
+    @GetMapping("/login-info")
+    @SecurityRequirement(name = "Authorization")
+    @Operation(summary = "회원 정보 조회(아이디, 닉네임, 이메일, 프로필 이미지) API")
+    public ResponseEntity<Object> findUserInfoById() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
-    @PostMapping("/login")
-    public ResponseEntity<SignResponseDto> login(@Valid @RequestBody SignRequestDto signRequestDto) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(userService.login(signRequestDto));
-    }
+        String userId;
 
-    @PostMapping("/logout")
-    public ResponseEntity<Void> logout(@Valid @RequestBody RefreshTokenRequestDto refreshTokenRequestDto) {
-        userService.logout(refreshTokenRequestDto.getRefreshToken());
-        return ResponseEntity.noContent().build();
-    }
+        if (principal instanceof org.springframework.security.core.userdetails.UserDetails) {
+            userId = ((org.springframework.security.core.userdetails.UserDetails) principal).getUsername();
+        } else {
+            // principal이 UserDetails가 아닌 경우 (ex: 익명 사용자)
+            return ResponseEntity.ok(new ResponseMessage<>(401, "인증되지 않은 사용자입니다.", null));
+        }
+        UserInfoResponseDto userInfo = userService.findUserInfoById(userId);
 
-    @PostMapping("/refresh")
-    @Operation(summary = "엑세스 토큰 재발급 API")
-    public ResponseEntity<ReissueTokenResponseDto> refresh(@Valid @RequestBody RefreshTokenRequestDto refreshTokenRequestDto) {
-        return ResponseEntity.ok(userService.refreshTokens(refreshTokenRequestDto.getRefreshToken()));
+        return ResponseEntity.ok(new ResponseMessage<>(200, "회원 정보 조회 성공", userInfo));
     }
-
 }
