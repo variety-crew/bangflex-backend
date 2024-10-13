915d782f1bfb9fd661d7411739b5fa10dc7495eb
Rohdamin <rohdamin3@gmail.com>
2024-10-13 16:25:09 +0900

    fix: 엑세스 토큰에 이메일, 닉네임 claim으로 저장하도록 수정

--- src/main/java/com/swcamp9th/bangflixbackend/security/jwt/JwtUtil.java
+++ src/main/java/com/swcamp9th/bangflixbackend/security/jwt/JwtUtil.java
@@ -45,8 +45,8 @@ public class JwtUtil {
 		Date now = new Date();
 		return BEARER_PREFIX + Jwts.builder()
 			.setSubject(member.getId())
-			.setSubject(member.getEmail())
-			.setSubject(member.getNickname())
+			.claim("email", member.getEmail())
+			.claim("nickname", member.getNickname())
 			.claim(AUTHORITIES_KEY, member.getRole())
 			.setIssuedAt(now)
 			.setExpiration(new Date(now.getTime() + accessTokenTime))
