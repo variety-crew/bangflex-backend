370ab0bb5a874c98ee752c0872e54f6a8c57fb1a
hoddeok07 <jhj2969@naver.com>
2024-10-17 16:37:42 +0900

    feat: 공지사항 게시글 목록 조회/상세 조회 기능 추가

--- src/main/java/com/swcamp9th/bangflixbackend/domain/noticePost/repository/NoticeFileRepository.java
+++ src/main/java/com/swcamp9th/bangflixbackend/domain/noticePost/repository/NoticeFileRepository.java
@@ -1,9 +1,14 @@
 package com.swcamp9th.bangflixbackend.domain.noticePost.repository;
 
 import com.swcamp9th.bangflixbackend.domain.noticePost.entity.NoticeFile;
+import com.swcamp9th.bangflixbackend.domain.noticePost.entity.NoticePost;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+
 @Repository("noticeFileRepository")
 public interface NoticeFileRepository extends JpaRepository<NoticeFile, Integer> {
+
+    Collection<NoticeFile> findByNoticePost(NoticePost noticePost);
 }
