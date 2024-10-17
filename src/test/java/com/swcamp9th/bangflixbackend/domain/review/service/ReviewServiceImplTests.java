ec805dc09cc8e9544f9e728d063d3e3630bee4f3
JeongJunSeo99 <wnstj0810@naver.com>
2024-10-17 14:03:36 +0900

    feat: 리뷰 서비스 test code 작성

--- src/test/java/com/swcamp9th/bangflixbackend/domain/review/service/ReviewServiceImplTests.java
+++ src/test/java/com/swcamp9th/bangflixbackend/domain/review/service/ReviewServiceImplTests.java
@@ -6,6 +6,7 @@ import com.swcamp9th.bangflixbackend.domain.review.dto.CreateReviewDTO;
 import com.swcamp9th.bangflixbackend.domain.review.entity.Review;
 import com.swcamp9th.bangflixbackend.domain.review.enums.Activity;
 import com.swcamp9th.bangflixbackend.domain.review.enums.Composition;
+import com.swcamp9th.bangflixbackend.domain.review.enums.HorrorLevel;
 import com.swcamp9th.bangflixbackend.domain.review.enums.Interior;
 import com.swcamp9th.bangflixbackend.domain.review.enums.Level;
 import com.swcamp9th.bangflixbackend.domain.review.enums.Probability;
@@ -31,69 +32,71 @@ import org.springframework.web.multipart.MultipartFile;
 
 @SpringBootTest
 public class ReviewServiceImplTests {
-//
-//    @Autowired
-//    private ReviewService reviewService;
-//
-//    @Autowired
-//    private PasswordEncoder passwordEncoder;
-//
-//    @MockBean
-//    private ReviewRepository reviewRepository;
-//
-//    @MockBean
-//    private ReviewThemeRepository reviewThemeRepository;
-//
-//    @MockBean
-//    private UserRepository userRepository;
-//
-//    @MockBean
-//    private ModelMapper modelMapper;
-//
-//    @MockBean
-//    private MultipartFile multipartFile;
-//
-//    @Test
-//    @Transactional
-//    public void testCreateReview() throws IOException, URISyntaxException {
-//        // given
-//        CreateReviewDTO newReviewDTO = new CreateReviewDTO();
-//        newReviewDTO.setThemeCode(1000);
-//        newReviewDTO.setActivity(Activity.ONE);
-//        newReviewDTO.setComposition(Composition.ONE);
-//        newReviewDTO.setContent("test");
-//        newReviewDTO.setInterior(Interior.ONE);
-//        newReviewDTO.setLevel(Level.ONE);
-//        newReviewDTO.setHeadcount(1);
-//        newReviewDTO.setProbability(Probability.FIVE);
-//        newReviewDTO.setLevel(Level.ONE);
-//
-//        String id = UUID.randomUUID().toString();
-//        Review mockReview = new Review();
-//        Theme mockTheme = new Theme();
-//        Member mockMember = Member.builder()
-//            .id(id)
-//            .isAdmin(false)
-//            .email(String.valueOf(UUID.randomUUID()))
-//            .password(passwordEncoder.encode(String.valueOf(UUID.randomUUID())))
-//            .nickname(String.valueOf(UUID.randomUUID()))
-//            .build();
-//
-//
-//        List<MultipartFile> images = List.of(multipartFile);
-//
-//        Mockito.when(modelMapper.map(newReviewDTO, Review.class)).thenReturn(mockReview);
-//        Mockito.when(reviewThemeRepository.findById(1000)).thenReturn(Optional.of(mockTheme));
-//        Mockito.when(userRepository.findById(id)).thenReturn(Optional.of(mockMember));
-//        Mockito.when(reviewRepository.save(mockReview)).thenReturn(mockReview);
-//
-//        // when
-//        reviewService.createReview(newReviewDTO, images, id);
-//
-//        // then
-//        Mockito.verify(reviewRepository, Mockito.times(1)).save(mockReview);
-//        Mockito.verify(userRepository, Mockito.times(1)).save(mockMember);
-//        assertEquals(mockTheme, mockReview.getTheme()); // 리뷰에 테마가 설정되었는지 확인
-//        assertEquals(mockMember, mockReview.getMember()); // 리뷰에 멤버가 설정되었는지 확인
-//    }
+
+    @Autowired
+    private ReviewService reviewService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @MockBean
+    private ReviewRepository reviewRepository;
+
+    @MockBean
+    private ReviewThemeRepository reviewThemeRepository;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private ModelMapper modelMapper;
+
+    @MockBean
+    private MultipartFile multipartFile;
+
+    @Test
+    @Transactional
+    public void testCreateReview() throws IOException, URISyntaxException {
+        // given
+        CreateReviewDTO newReviewDTO = new CreateReviewDTO();
+        newReviewDTO.setThemeCode(1000);
+        newReviewDTO.setActivity(Activity.ONE);
+        newReviewDTO.setComposition(Composition.ONE);
+        newReviewDTO.setContent("test");
+        newReviewDTO.setInterior(Interior.ONE);
+        newReviewDTO.setLevel(Level.ONE);
+        newReviewDTO.setHeadcount(1);
+        newReviewDTO.setProbability(Probability.FIVE);
+        newReviewDTO.setHorrorLevel(HorrorLevel.ONE);
+        newReviewDTO.setTakenTime(60);
+        newReviewDTO.setTotalScore(6);
+
+        String id = UUID.randomUUID().toString();
+        Review mockReview = new Review();
+        Theme mockTheme = new Theme();
+        Member mockMember = Member.builder()
+            .id(id)
+            .isAdmin(false)
+            .email(String.valueOf(UUID.randomUUID()))
+            .password(passwordEncoder.encode(String.valueOf(UUID.randomUUID())))
+            .nickname(String.valueOf(UUID.randomUUID()))
+            .build();
+
+
+        List<MultipartFile> images = null;
+
+        Mockito.when(modelMapper.map(newReviewDTO, Review.class)).thenReturn(mockReview);
+        Mockito.when(reviewThemeRepository.findById(1000)).thenReturn(Optional.of(mockTheme));
+        Mockito.when(userRepository.findById(id)).thenReturn(Optional.of(mockMember));
+        Mockito.when(reviewRepository.save(mockReview)).thenReturn(mockReview);
+
+        // when
+        reviewService.createReview(newReviewDTO, images, id);
+
+        // then
+        Mockito.verify(reviewRepository, Mockito.times(1)).save(mockReview);
+        Mockito.verify(userRepository, Mockito.times(1)).save(mockMember);
+        assertEquals(mockTheme, mockReview.getTheme()); // 리뷰에 테마가 설정되었는지 확인
+        assertEquals(mockMember, mockReview.getMember()); // 리뷰에 멤버가 설정되었는지 확인
+    }
 }
