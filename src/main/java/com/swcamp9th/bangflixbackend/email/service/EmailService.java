056502e6b4e4d103cf79e1342f4c045488a4322c
Rohdamin <rohdamin3@gmail.com>
2024-10-17 17:46:34 +0900

    feat: EmailService 추가

---
+++ src/main/java/com/swcamp9th/bangflixbackend/email/service/EmailService.java
@@ -0,0 +1,95 @@
+package com.swcamp9th.bangflixbackend.email.service;
+
+import com.swcamp9th.bangflixbackend.domain.user.dto.EmailCodeRequestDto;
+import com.swcamp9th.bangflixbackend.redis.RedisService;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
+import java.util.Random;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class EmailService {
+
+    private final JavaMailSender javaMailSender;
+    private final RedisService redisService;
+
+    @Value("${mail.username}")
+    private static String senderEmail;
+
+    @Transactional
+    public String createNumber() {
+        Random random = new Random();
+        StringBuilder key = new StringBuilder();
+
+        for (int i = 0; i < 8; i++) { // 인증 코드 8자리
+            int index = random.nextInt(3); // 0~2까지 랜덤, 랜덤값으로 switch문 실행
+
+            switch (index) {
+                case 0 -> key.append((char) (random.nextInt(26) + 97)); // 소문자
+                case 1 -> key.append((char) (random.nextInt(26) + 65)); // 대문자
+                case 2 -> key.append(random.nextInt(10)); // 숫자
+            }
+        }
+        return key.toString();
+    }
+
+    @Transactional
+    public MimeMessage createMail(String mail, String number) throws MessagingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+
+        message.setFrom(senderEmail);
+        message.setRecipients(MimeMessage.RecipientType.TO, mail);
+        message.setSubject("이메일 인증");
+        String body = "";
+        body += "<h3>요청하신 인증 번호입니다.</h3>";
+        body += "<h1>" + number + "</h1>";
+        body += "<h3>감사합니다.</h3>";
+        message.setText(body, "UTF-8", "html");
+
+        return message;
+    }
+
+    // 메일 발송
+    @Transactional
+    public String sendSimpleMessage(String sendEmail) throws MessagingException {
+        String number = createNumber(); // 랜덤 인증번호 생성
+
+        MimeMessage message = createMail(sendEmail, number); // 메일 생성
+        try {
+            javaMailSender.send(message); // 메일 발송
+        } catch (MailException e) {
+            e.printStackTrace();
+            throw new IllegalArgumentException("메일 발송 중 오류가 발생했습니다.");
+        }
+
+        // 레디스에 저장
+        redisService.saveEmailCode(sendEmail, number);
+
+        return number; // 생성된 인증번호 반환
+    }
+
+    @Transactional
+    public boolean findEmailcode(EmailCodeRequestDto emailCodeRequestDto) {
+        // redis에 저장된 email 조회
+        log.info("*** EmailService findEmailcode - emailCodeRequestDto.getEmail(): {}", emailCodeRequestDto.getEmail());
+
+        String redisEmailCode = redisService.getEmailCode(emailCodeRequestDto.getEmail());
+
+        log.info("*** EmailService findEmailcode - emailCodeRequestDtogetCode(): {}", emailCodeRequestDto.getCode());
+        log.info("*** EmailService findEmailcode - redisEmailCode: {}", redisEmailCode);
+        // redis에 저장된 email, code 삭제
+        redisService.deleteEmailCode(emailCodeRequestDto.getEmail());
+
+        return Objects.equals(redisEmailCode, emailCodeRequestDto.getCode());
+    }
+}
