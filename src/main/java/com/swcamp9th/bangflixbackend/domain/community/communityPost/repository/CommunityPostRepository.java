4fcdea3bf68cb154199a977a489b34e3d639fab1
hoddeok07 <jhj2969@naver.com>
2024-10-17 12:26:19 +0900

    fix: 커뮤니티 게시글 목록 조회 기능 수정(페이지네이션 X)

--- src/main/java/com/swcamp9th/bangflixbackend/domain/community/communityPost/repository/CommunityPostRepository.java
+++ src/main/java/com/swcamp9th/bangflixbackend/domain/community/communityPost/repository/CommunityPostRepository.java
@@ -3,11 +3,16 @@ package com.swcamp9th.bangflixbackend.domain.community.communityPost.repository;
 import com.swcamp9th.bangflixbackend.domain.community.communityPost.entity.CommunityPost;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository("communityPostRepository")
 public interface CommunityPostRepository extends JpaRepository<CommunityPost, Integer> {
 
     Page<CommunityPost> findByActiveTrue(Pageable pageable);
+
+    List<CommunityPost> findByActiveTrue(Sort createdAt);
 }
