0dba15bc87f1783dcc4d31197fcfdbc381ea5fe9
JeongJunSeo99 <wnstj0810@naver.com>
2024-10-18 11:57:25 +0900

    fix: 테마 일부 API에 대해 권한 필터링 적용 안함

--- src/main/java/com/swcamp9th/bangflixbackend/config/WebSecurityConfig.java
+++ src/main/java/com/swcamp9th/bangflixbackend/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import com.swcamp9th.bangflixbackend.security.user.UserDetailsServiceImpl;
 import jakarta.servlet.Filter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -66,6 +67,8 @@ public class WebSecurityConfig {
 			authorize
 				.requestMatchers("/api/v1/auth/**").permitAll()
 				.requestMatchers("/swagger-ui/**", "/v3/**").permitAll()
+				.requestMatchers(HttpMethod.GET, "/api/v1/themes/week").permitAll()
+				.requestMatchers(HttpMethod.GET, "/api/v1/themes").permitAll()
 				.anyRequest().authenticated()
 		);
 
