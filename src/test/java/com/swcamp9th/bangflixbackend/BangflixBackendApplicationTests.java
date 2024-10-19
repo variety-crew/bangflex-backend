7c0a41e861fed8552a1319c338e07c07cc563c6e
gilhan <rlfgks97@gmail.com>
2024-10-19 17:38:58 +0900

    feat: 테스트 프로필 분리

--- src/test/java/com/swcamp9th/bangflixbackend/BangflixBackendApplicationTests.java
+++ src/test/java/com/swcamp9th/bangflixbackend/BangflixBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.swcamp9th.bangflixbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("prod") // application-{}.yml 에 맞게 설정
 class BangflixBackendApplicationTests {
 
     @Test
