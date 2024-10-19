38ffe8c18e9874a5ee0314404884c0b809a9d280
gilhan <rlfgks97@gmail.com>
2024-10-19 21:55:33 +0900

    build: pipeline 수정

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM openjdk:17-alpine
 COPY build/libs/*.jar app.jar
+COPY src/main/resources/application-prod.yml application.yml
 ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/test/java/com/swcamp9th/bangflixbackend/BangflixBackendApplicationTests.java
+++ src/test/java/com/swcamp9th/bangflixbackend/BangflixBackendApplicationTests.java
@@ -5,7 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("local") // application-{}.yml 에 맞게 설정
 class BangflixBackendApplicationTests {
 
     @Test
